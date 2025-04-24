2e15542ea807d95189f05850d16d0bf79bd7eb0d
mayumihb <mayumihb80@gmail.com>
2025-04-23 21:01:03 -0300

    feat: changed register screen appearence

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:app_atletica/widgets/custom_button.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -20,18 +21,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
         isLoading = true;
       });
 
-      // Simulação de registro (substitua por chamada de API)
       Future.delayed(Duration(seconds: 2), () {
         setState(() {
           isLoading = false;
         });
 
-        // Exemplo: Mostrar um snackbar ao concluir o registro
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Registro realizado com sucesso!')),
         );
 
-        Navigator.pop(context); // Volta para a tela de login
+        Navigator.pop(context);
       });
     }
   }
@@ -39,99 +38,148 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Registrar-se',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/brasao.png'),
+                  fit: BoxFit.contain,
                 ),
               ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: nameController,
-                decoration: InputDecoration(
-                  hintText: 'Nome Completo',
-                  prefixIcon: Icon(Icons.person, color: Colors.white),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira seu nome';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  prefixIcon: Icon(Icons.email, color: Colors.white),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira seu email';
-                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                    return 'Insira um email válido';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Senha',
-                  prefixIcon: Icon(Icons.lock, color: Colors.white),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira sua senha';
-                  } else if (value.length < 6) {
-                    return 'A senha deve ter pelo menos 6 caracteres';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: confirmPasswordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Confirme sua senha',
-                  prefixIcon: Icon(Icons.lock, color: Colors.white),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, confirme sua senha';
-                  } else if (value != passwordController.text) {
-                    return 'As senhas não correspondem';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              isLoading
-                  ? CircularProgressIndicator(color: Theme.of(context).colorScheme.secondary)
-                  : CustomButton(
-                      text: 'Registrar-se',
-                      onPressed: _register,
-                    ),
-              TextButton(
-                onPressed: () => Navigator.pushNamed(context, '/'),
-                child: Text('Login', style: TextStyle(color: Colors.white)),
-              ),
-            ],
+            ),
           ),
-        ),
+          Container(
+            color: const Color.fromARGB(178, 1, 28, 58),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/aaabe.png',
+                      scale: 0.8,
+                    ),
+                    SizedBox(height: 60),
+                    Text(
+                      'Registrar-se',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(height: 60),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome Completo',
+                        prefixIcon: Icon(Icons.person, color: Colors.white),
+                        border: UnderlineInputBorder(),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.amber, width: 2),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira seu nome';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 50),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email, color: Colors.white),
+                        border: UnderlineInputBorder(),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.amber, width: 2),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira seu email';
+                        } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                          return 'Insira um email válido';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 50),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                        border: UnderlineInputBorder(),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.amber, width: 2),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira sua senha';
+                        } else if (value.length < 6) {
+                          return 'A senha deve ter pelo menos 6 caracteres';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 50),
+                    TextFormField(
+                      controller: confirmPasswordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Confirme sua senha',
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, confirme sua senha';
+                        } else if (value != passwordController.text) {
+                          return 'As senhas não correspondem';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 60),
+                    isLoading
+                        ? CircularProgressIndicator(
+                            color: Theme.of(context).colorScheme.secondary,
+                          )
+                        : CustomButton(
+                            text: 'Registrar-se',
+                            onPressed: _register,
+                          ),
+                    TextButton(
+                      onPressed: () => Navigator.pushNamed(context, '/'),
+                      child: Text('Já tem uma conta? Login'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
